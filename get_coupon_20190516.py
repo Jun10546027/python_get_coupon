e37aaa63bcf110f6222feb77745a634a057697bf
10546027Jun <10546027@ntub.edu.tw>
2019-05-19 13:50:14 +0800

    '修改變數名稱，刪減不必要東西'

--- get_coupon_20190516.py
+++ get_coupon_20190516.py
@@ -18,31 +18,38 @@ df1 = pd.DataFrame(columns=['title','adress'])
 add_String=''
 add_Tag = ''
 
-todayjudge = ''
+today_judge = ''
+
+#判斷檔名
+def judge(x):
+    if "jpg" in x or "png" in x:
+        return True
+    else:
+        return False
 
 
 for _ in main:
-    indextitle = _.find("a").string  # 這個會有\n
-    indexurl = _.find("a").get("href")
-    IndexTitle = indextitle.split('\n')[1]
+    index_title = _.find("a").string  # 這個會有\n
+    index_url = _.find("a").get("href")
+
+    #直接使用網站的title會有\n的問題，所以要先排除\n或\造成的錯誤("\"會造成註解)
+    Index_Title = index_title.split('\n')[1]
     #judge the url
-    todayjudge  = indexurl
+    today_judge  = index_url
 
-    indexrespond = requests.get(indexurl, headers={'User-agent': 'Mozilla/5.0'})
-    # print(indexrespond)
-    indexhtml = BeautifulSoup(indexrespond.text)
-    indexmain = indexhtml.find_all("a", attrs={'style': 'margin-left: 1em; margin-right: 1em;'})
-    # print(IndexTitle)  <-直接使用網站的title會有\n的問題，所以要先排除\n或\造成的錯誤("\"會造成註解)
-    # print(indexmain[0].get("href"))  #<--這裡是get,因為已經在<a>裡了
-
+    index_respond = requests.get(index_url, headers={'User-agent': 'Mozilla/5.0'})
+    index_html = BeautifulSoup(index_respond.text)
+    index_main = index_html.find_all("a", attrs={'style': 'margin-left: 1em; margin-right: 1em;'})
+    
     # 網頁內容
-    index_content = indexhtml.find_all("span", attrs={'style': 'font-size: large;'})
+    index_content = index_html.find_all("span", attrs={'style': 'font-size: large;'})
+    
     #網頁 tag
-    indextag = indexhtml.find_all("div", class_="widget-tags")
+    index_tag = index_html.find_all("div", class_="widget-tags")
 
-    #class
-    for text in indextag:
-        print(IndexTitle)
+    #MySQL Myclass
+    for text in index_tag:
+        print(Index_Title)
         text_a = text.find_all("a")
 
         for str in text_a:
@@ -52,30 +59,30 @@ for _ in main:
             add_Tag = add_Tag + web_tag + "/"
 
 
-    #content
+    #MySQL content
     for text in index_content:
         if text.string is not None:
             # add_text.append(text.string.splitlines())
             #splitlines將\n分開
             add_String = add_String + text.string.splitlines()[0]+"\ "
-    # ID
-    for i in indexmain:
+    
+    #MySQL ID
+    for i in index_main:
         downloadURL = i.get("href")
         ds = downloadURL.split("/")
         ##設定ID
         coupon_id = ds[5]
-        s = pd.Series([coupon_id,IndexTitle.splitlines()[-1],add_Tag, add_String], index=['id','title','class','content'])
+        s = pd.Series([coupon_id,Index_Title.splitlines()[-1],add_Tag, add_String], index=['id','title','class','content'])
         df = df.append(s, ignore_index=True)
 
-    # 重製儲存字串
+    #重製儲存字串
     add_String = ''
     add_Tag = ''
 
     #儲存圖片
-    for i in indexmain:
-        #print(IndexTitle)
+    for i in index_main:
         # 設定資料夾路徑，怕標題有/
-        dname = "C:/Users/case-pc/Desktop/coupon/" + IndexTitle.split("/")[0] + "/"
+        dname = "C:/Users/Jun/Desktop/coupon/" + Index_Title.split("/")[0] + "/"
 
         if not os.path.exists(dname):
             os.mkdir(dname)
@@ -85,20 +92,15 @@ for _ in main:
         ds = downloadURL.split("/")
         filetype = ds[-1].split(".")[-1]
         judgeURL = filetype
-        # print(ds[5])
+       
         if judgeURL:
             fpath = dname + ds[5] + "." + filetype
             urlretrieve(downloadURL, fpath)
-        s1 = pd.Series([IndexTitle.splitlines()[-1],fpath],index=['title','adress'])
+        
+	s1 = pd.Series([Index_Title.splitlines()[-1],fpath],index=['title','adress'])
         df1 = df1.append(s1,ignore_index=True)
 
-print("判斷標準",todayjudge)
-
-def judge(x):
-    if "jpg" in x or "png" in x:
-        return True
-    else:
-        return False
+print("判斷標準",today_judge)
 
 import datetime
 now = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -112,43 +114,36 @@ while d >= end:
     print(d.strftime("%Y-%m-%d"))
     url = "https://www.4freeapp.com/search?updated-max="+d.strftime("%Y-%m-%d")+"T10%3A53%3A00%2B08%3A00&max-results=7#PageNo=2"
     respond = requests.get(url, headers={'User-agent': 'Mozilla/5.0'})
-    # print(respond)
 
     html = BeautifulSoup(respond.text)
     main = html.find_all("div", class_="caption")
-    # print(main[0].find("a").get("href"))
 
     # 網頁內容
-    index_content = indexhtml.find_all("span", attrs={'style': 'font-size: large;'})
+    index_content = index_html.find_all("span", attrs={'style': 'font-size: large;'})
     # 網頁 tag
-    indextag = indexhtml.find_all("div", class_="widget-tags")
+    index_tag = index_html.find_all("div", class_="widget-tags")
 
 
     for times in reversed(main):
-        indextitle = times.find("a").string #這個會有\n
-        indexurl = times.find("a").get("href")
-        IndexTitle = indextitle.split('\n')[1]  ##把indextitle的\n排除
-        # print(indextitle)
-        # print(indexurl)
-
-        if todayjudge == indexurl:
-            todayjudge  = main[-1].find("a").get("href")
+        index_title = times.find("a").string #這個會有\n
+        index_url = times.find("a").get("href")
+        Index_Title = index_title.split('\n')[1]  ##把indextitle的\n排除
+        
+        if today_judge == index_url:
+            today_judge  = main[-1].find("a").get("href")
             break
 
 
         print("頁尾",main[-1].find("a").get("href"))
-        print("判斷標準",todayjudge)
-        print(IndexTitle)
+        print("判斷標準",today_judge)
+        print(Index_Title)
 
-        indexrespond = requests.get(indexurl,headers={'User-agent': 'Mozilla/5.0'})
-        # print(indexrespond)
-        indexhtml = BeautifulSoup(indexrespond.text)
-        indexmain = indexhtml.find_all("a",attrs = {'style':'margin-left: 1em; margin-right: 1em;'})
-        # print(IndexTitle)  <-直接使用網站的title會有\n的問題，所以要先排除\n或\造成的錯誤("\"會造成註解)
-        # print(indexmain[0].get("href"))  <--這裡是get,因為已經在<a>裡了
+        index_respond = requests.get(index_url,headers={'User-agent': 'Mozilla/5.0'})
+        index_html = BeautifulSoup(index_respond.text)
+        index_main = index_html.find_all("a",attrs = {'style':'margin-left: 1em; margin-right: 1em;'})
 
         # 網頁內容
-        index_content = indexhtml.find_all("span", attrs={'style': 'font-size: large;'})
+        index_content = index_html.find_all("span", attrs={'style': 'font-size: large;'})
 
         # 將標題和內容存成CSV
         for text in index_content:
@@ -157,9 +152,9 @@ while d >= end:
                 # splitlines將\n分開
                 add_String = add_String + text.string.splitlines()[0] + "\ "
 
-        # class
-        for text in indextag:
-            print(IndexTitle)
+        #MySQL Myclass
+        for text in index_tag:
+            print(Index_Title)
             text_a = text.find_all("a")
 
             for str in text_a:
@@ -167,14 +162,14 @@ while d >= end:
                 class_tab = str.string
                 web_tag = class_tab.split("\n")[1]
                 add_Tag = add_Tag + web_tag + "/"
-        # ID
-        for i in indexmain:
+        #MySQL ID
+        for i in index_main:
             downloadURL = i.get("href")
             ds = downloadURL.split("/")
             ##設定ID
             coupon_id = ds[5]
             print(coupon_id)
-            s = pd.Series([coupon_id, IndexTitle.splitlines()[-1], add_Tag, add_String],
+            s = pd.Series([coupon_id, Index_Title.splitlines()[-1], add_Tag, add_String],
                           index=['id', 'title', 'class', 'content'])
             df = df.append(s, ignore_index=True)
 
@@ -183,10 +178,10 @@ while d >= end:
         add_Tag = ''
 
         ##儲存
-        for i in indexmain:
-            print(IndexTitle)
+        for i in index_main:
+            print(Index_Title)
             # 設定資料夾路徑，怕標題有/
-            dname = "C:/Users/case-pc/Desktop/coupon/" + IndexTitle.split("/")[0] + "/"
+            dname = "C:/Users/Jun/Desktop/coupon/" + Index_Title.split("/")[0] + "/"
             if not os.path.exists(dname):
                 os.mkdir(dname)
 
@@ -195,11 +190,11 @@ while d >= end:
 
             ds = downloadURL.split("/")
             filetype = ds[-1].split(".")[-1]
-            # print(ds[5])
             if judge(filetype):
                 fpath = dname + ds[5] + "." + filetype
                 urlretrieve(downloadURL, fpath)
-            s1 = pd.Series([IndexTitle.splitlines()[-1], fpath], index=['title', 'adress'])
+            
+	    s1 = pd.Series([Index_Title.splitlines()[-1], fpath], index=['title', 'adress'])
             df1 = df1.append(s1, ignore_index=True)
 
     d -= delta
